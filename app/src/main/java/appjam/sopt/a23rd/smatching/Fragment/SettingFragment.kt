edefa80aa715f50295c1886beda84d2bc3a8d94b
genieee23 <jwjin0823@gmail.com>
2018-12-30 04:17:07 +0900

    error

--- app/src/main/java/appjam/sopt/a23rd/smatching/Fragment/SettingFragment.kt
+++ app/src/main/java/appjam/sopt/a23rd/smatching/Fragment/SettingFragment.kt
@@ -1,17 +1,16 @@
 package appjam.sopt.a23rd.smatching.Fragment
 
+import android.app.Fragment
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class MyPageFragment : Fragment(){
-
+class SettingFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_my_page, container, false)
+        return inflater.inflate(R.layout.fragment_setting, container, false)
     }
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
     }
 }
