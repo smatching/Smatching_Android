c67453ee46c8f0d868890b23ec5fc5ce6bfe823a
jhm6258680 <jhm6258680@naver.com>
2019-01-01 03:57:33 +0900

    수정중

--- app/src/main/java/appjam/sopt/a23rd/smatching/Fragment/CustomConditionClickFragment.kt
+++ app/src/main/java/appjam/sopt/a23rd/smatching/Fragment/CustomConditionClickFragment.kt
@@ -22,6 +22,7 @@ import org.jetbrains.anko.support.v4.toast
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.*
 
 class CustomConditionClickFragment : Fragment(){
     val dataList : ArrayList<CondSummaryListData> by lazy {
@@ -30,7 +31,6 @@ class CustomConditionClickFragment : Fragment(){
     val networkService: NetworkService by lazy {
         ApplicationController.instance.networkService
     }
-    var test:Int = 1
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_custom_condition_click, container, false)
     }
@@ -39,8 +39,8 @@ class CustomConditionClickFragment : Fragment(){
         fragment_custom_condition_click_iv_back.setOnClickListener {
             replaceFragment(CustomConditionNotClickFragment())
         }
-        //getUserSmatchingCondResponse()
-        testResponse()
+        getUserSmatchingCondResponse()
+        //testResponse()
     }
     private fun replaceFragment(fragment : Fragment) {
         val transaction : FragmentTransaction = fragmentManager!!.beginTransaction()
@@ -75,7 +75,123 @@ class CustomConditionClickFragment : Fragment(){
 
             override fun onResponse(call: Call<GetSmatchingListResponse>, response: Response<GetSmatchingListResponse>) {
                 if (response.isSuccessful) {
-                    toast("테스트")
+                    val locationList=  Arrays.asList("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "")
+                    
+                    if(response.body()!!.data.location.jeonbuk)
+                        locationList[0] = "전북"
+                    else
+                        locationList[0] = ""
+
+                    if(response.body()!!.data.location.gangwon)
+                        locationList[1] = "강원"
+                    else
+                        locationList[1] = ""
+
+                    if(response.body()!!.data.location.gwangju)
+                        locationList[2] = "광주"
+                    else
+                        locationList[2] = ""
+
+                    if(response.body()!!.data.location.ulsan)
+                        locationList[3] = "울산"
+                    else
+                        locationList[3] = ""
+
+                    if(response.body()!!.data.location.kyungbuk)
+                        locationList[4] = "경북"
+                    else
+                        locationList[4] = ""
+
+                    if(response.body()!!.data.location.sejong)
+                        locationList[5] = "세종"
+                    else
+                        locationList[5] = ""
+
+                    if(response.body()!!.data.location.chungbuk)
+                        locationList[6] = "충북"
+                    else
+                        locationList[6] = ""
+
+                    if(response.body()!!.data.location.kyungnam)
+                        locationList[7] = "경남"
+                    else
+                        locationList[7] = ""
+
+                    if(response.body()!!.data.location.seoul)
+                        locationList[8] = "서울"
+                    else
+                        locationList[8] = ""
+
+                    if(response.body()!!.data.location.chungnam)
+                        locationList[9] = "충남"
+                    else
+                        locationList[9] = ""
+
+                    if(response.body()!!.data.location.daejeon)
+                        locationList[10] = "대전"
+                    else
+                        locationList[10] = ""
+
+                    if(response.body()!!.data.location.busan)
+                        locationList[11] = "부산"
+                    else
+                        locationList[11] = ""
+
+                    if(response.body()!!.data.location.jeju)
+                        locationList[12] = "제주"
+                    else
+                        locationList[12] = ""
+
+                    if(response.body()!!.data.location.daegu)
+                        locationList[13] = "대구"
+                    else
+                        locationList[13] = ""
+
+                    if(response.body()!!.data.location.kyunggi)
+                        locationList[14] = "경기"
+                    else
+                        locationList[14] = ""
+
+                    if(response.body()!!.data.location.incheon)
+                        locationList[15] = "인천"
+                    else
+                        locationList[15] = ""
+
+                    if(response.body()!!.data.location.jeonnam)
+                        locationList[16] = "전남"
+                    else
+                        locationList[16] = ""
+
+                    if(response.body()!!.data.location.domesticAll)
+                        locationList[17] = "국내전체"
+                    else
+                        locationList[17] = ""
+                    if(response.body()!!.data.location.aborad)
+                        locationList[18] = "국외"
+                    else
+                        locationList[18] = ""
+                    if(locationList[17] != ""){
+                        for (a in 0..16)
+                            locationList[a] = ""
+                        if(locationList[18] != "")
+                            locationList[18] = "@" + locationList[18]
+                    }
+                    else{
+                        for (a in 0..18) {
+                            var first: Int = 0
+                            if (locationList[a] != "" && a != first)
+                                locationList[a] = "@" + locationList[a]
+                            else
+                                first++
+                        }
+                    }
+                    fragment_custom_condition_click_tv_location.text = locationList.toString()
+                            .replace(",", "")  //remove the commas
+                            .replace("@", ", ")  //add the commas
+                            .replace(" ,", ", ")
+                            .replace("[", "")  //remove the right bracket
+                            .replace("]", "")  //remove the left bracket
+                            .trim()
                 }
             }
         })
--- app/src/main/java/appjam/sopt/a23rd/smatching/Get/GetSmatchingListResponse.kt
+++ app/src/main/java/appjam/sopt/a23rd/smatching/Get/GetSmatchingListResponse.kt
@@ -11,11 +11,102 @@ data class CondData(
         val condIdx : Int,
         val condName : String,
         val alert : Boolean,
-        val location : ArrayList<LocationData>,
-        val age : ArrayList<AgeData>,
-        val period : ArrayList<PeriodData>,
-        val field : ArrayList<FieldData>,
-        val advantage : ArrayList<AdvantageData>,
-        val busiType : ArrayList<BusiTypeData>,
-        val excCategory : ArrayList<ExcCategoryData>
+        val location : LocationData,
+        val age : AgeData,
+        val period : PeriodData,
+        val field : FieldData,
+        val advantage : AdvantageData,
+        val busiType : BusiTypeData,
+        val excCategory : ExcCategoryData
+)
+
+data class LocationData(
+        val jeonbuk: Boolean,
+        val gangwon: Boolean,
+        val gwangju: Boolean,
+        val ulsan: Boolean,
+        val kyungbuk: Boolean,
+        val sejong: Boolean,
+        val chungbuk: Boolean,
+        val kyungnam: Boolean,
+        val seoul: Boolean,
+        val chungnam: Boolean,
+        val daejeon: Boolean,
+        val busan: Boolean,
+        val jeju: Boolean,
+        val daegu: Boolean,
+        val aborad: Boolean,
+        val kyunggi: Boolean,
+        val incheon: Boolean,
+        val jeonnam: Boolean,
+        val domesticAll: Boolean
+)
+data class AgeData(
+        val forty_more: Boolean,
+        val twenty_less: Boolean,
+        val twenty_forty: Boolean
+)
+data class PeriodData(
+        val three_four: Boolean,
+        val six_seven: Boolean,
+        val one_two: Boolean,
+        val seven_more: Boolean,
+        val zero_one: Boolean,
+        val four_five: Boolean,
+        val yet: Boolean,
+        val two_three: Boolean,
+        val five_six: Boolean
+)
+data class FieldData(
+        val a: Boolean,
+        val b: Boolean,
+        val c: Boolean,
+        val d: Boolean,
+        val e: Boolean,
+        val f: Boolean,
+        val g: Boolean,
+        val h: Boolean,
+        val i: Boolean,
+        val j: Boolean,
+        val k: Boolean,
+        val l: Boolean,
+        val m: Boolean,
+        val n: Boolean,
+        val o: Boolean,
+        val p: Boolean,
+        val q: Boolean,
+        val r: Boolean,
+        val s: Boolean,
+        val t: Boolean,
+        val u: Boolean,
+        val v: Boolean
+)
+data class AdvantageData(
+        val sole: Boolean,
+        val univ: Boolean,
+        val woman: Boolean,
+        val social: Boolean,
+        val disabled: Boolean,
+        val togather: Boolean,
+        val fourth: Boolean,
+        val retry: Boolean
+)
+data class BusiTypeData(
+        val sole: Boolean,
+        val small: Boolean,
+        val big: Boolean,
+        val pre: Boolean,
+        val midsmall: Boolean,
+        val midbig: Boolean,
+        val tradi: Boolean
+)
+data class ExcCategoryData(
+        val loan: Boolean,
+        val edu: Boolean,
+        val know: Boolean,
+        val global: Boolean,
+        val place: Boolean,
+        val make: Boolean,
+        val local: Boolean,
+        val gov: Boolean
 )
