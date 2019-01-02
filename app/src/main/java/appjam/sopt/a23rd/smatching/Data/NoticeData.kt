2630ff97928373708b9762a3df0a84050c3b1dbd
genieee23 <jwjin0823@gmail.com>
2019-01-03 03:13:13 +0900

    new repository

--- app/src/main/java/appjam/sopt/a23rd/smatching/Data/NoticeData.kt
+++ app/src/main/java/appjam/sopt/a23rd/smatching/Data/NoticeData.kt
@@ -5,5 +5,6 @@ data class NoticeData(
         val title: String,
         val institution : String,
         val dday: Int,
-        val scrap : Int
+        val scrap : Int,
+        val readCnt : Int
 )
